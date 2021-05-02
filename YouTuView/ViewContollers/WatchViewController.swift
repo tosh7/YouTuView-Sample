03cdeb328e8433954271f8b1864ab5abbe07ce40
tosh7 <zlia.6.lj.425@gmail.com>
2021-05-02 16:29:15 +0900

    setup WatchViewController

--- YouTuView/ViewContollers/WatchViewController.swift
+++ YouTuView/ViewContollers/WatchViewController.swift
@@ -11,5 +11,29 @@ final class WatchViewController: UIViewController {
     required init?(coder: NSCoder) { fatalError() }
 
     private let video: VideoInfo
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        setupViews()
+    }
+
+    private func setupViews() {
+        self.view.addSubview(videoPlayerImageView)
+
+        videoPlayerImageView.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.topMargin)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(videoPlayerImageView.snp.width).multipliedBy(720.0 / 1280.0)
+        }
+    }
+
+    private lazy var videoPlayerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = self.video.thumbnail
+        return imageView
+    }()
 }
 
